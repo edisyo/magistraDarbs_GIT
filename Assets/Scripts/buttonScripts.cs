019f1404c9d54a705d66e9d89a1061588e45fb48
Eduards Jesikens <45883565+edisyo@users.noreply.github.com>
2021-12-20 13:28:58 +0200

    Main menu UI built
    made main menu UI with UI Builder. Added background, buttons and scripts to make them work. Need to test them now on iPhone

--- Assets/Scripts/buttonScripts.cs
+++ Assets/Scripts/buttonScripts.cs
@@ -2,13 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class buttonScripts : MonoBehaviour
 {
+    private Button btn_2d;
+    private Button btn_3d;
+    private Button btn_exit;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        var root = GetComponent<UIDocument>().rootVisualElement;
+
+        btn_2d = root.Q<Button>("2dButton");
+        btn_3d = root.Q<Button>("3dButton");
+        btn_exit = root.Q<Button>("exitButton");
     }
 
     // Update is called once per frame
@@ -20,5 +29,20 @@ public class buttonScripts : MonoBehaviour
     public void LoadScene(string sceneNameToGoBack)
     {
         SceneManager.LoadScene(sceneNameToGoBack, LoadSceneMode.Single);
+    }
+
+    public void Button2DPressed()
+    {
+        SceneManager.LoadScene("2D_bodyTracking", LoadSceneMode.Single);
+    }
+
+    public void Button3DPressed()
+    {
+        SceneManager.LoadScene("3D_bodyTracking", LoadSceneMode.Single);
+    }
+
+    public void ButtonExitPressed()
+    {
+        Application.Quit();
     }
 }
